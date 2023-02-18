a362e5b52cb36ba4500b83701d584744147f0fa7
Никита Фомин <defdXs@yandex.ru>
2023-02-18 17:57:06 +0300

    Fixed lab2

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,5 @@
 ﻿#include "Lib.h"
+#include <windows.h>
 
 
 using namespace SpecialFunctionsForLabs;
@@ -9,31 +10,32 @@ using namespace arrf;
 int main() {
 	setlocale(LC_ALL, "Russian");
 	setlocale(LC_NUMERIC, "C");
-	srand(time(NULL));
+	SetConsoleCP(1251);
+
 
 	cout << "|| Лабораторная работа №1 (сем. 2)\t||\n|| Вариант №20\t\t\t\t||\n|| Группа: ВПР12\t\t\t||\n|| Студент: Фомин Н. А.\t\t\t||\n\n";
 
 
 	//                                              ЗАДАЧА №1
-	//cout << "| Задача №1 |\n\n";
+	cout << "| Задача №1 |\n\n";
 
-	//cout << "Для начала найдем граничные коды заданных областей:" << endl;
-	//cout << "a-z: " << (int)'a' << " -- " << (int)'z' << endl;
-	//cout << "а-я: " << (int)'а' << " -- " << (int)'я' << endl;
-	//cout << "А-Я: " << (int)'А' << " -- " << (int)'Я' << endl << endl;
+	cout << "Для начала найдем граничные коды заданных областей:" << endl;
+	cout << "a-z: " << (int)'a' << " -- " << (int)'z' << endl;
+	cout << "а-я: " << (int)'а' << " -- " << (int)'я' << endl;
+	cout << "А-Я: " << (int)'А' << " -- " << (int)'Я' << endl << endl;
 
-	//cout << "Введите строку: ";
-	//string str;	getline(cin, str);
-	//cout << "\n\n";
-	//
-	//// счетчик символов
-	//unsigned int counter = 0;
+	cout << "Введите строку: ";
+	string str;	getline(cin, str);
+	cout << "\n\n";
+	
+	// счетчик символов
+	unsigned int counter = 0;
 
-	//for (int c : str)
-	//	if ((97 <= c && c <= 122) || (-96 <= c && c <= -17) || (-128 <= c && c <= -97))
-	//		counter++;
+	for (int c : str)
+		if ((97 <= c && c <= 122) || (-32 <= c && c <= -1) || (-64 <= c && c <= -33))
+			counter++;
 
-	//cout << "Количество символов по условию задачи: " << counter << "\n\n\n";
+	cout << "Количество символов по условию задачи: " << counter << "\n\n\n";
 
 	//                                              ЗАДАЧА №2
 	cout << "| Задача №1 |\n\n";
@@ -46,9 +48,8 @@ int main() {
 		getline(cin, line);
 		text += line + "\n";
 	}
-	cout << text;
 
-	char separator = input<char>("Введите символ для удаления: ");
+	char separator = input<char>("\nВведите символ для удаления: ");
 
 	// удаление символа(ов) из текста
 	for (int i = text.size()-1; i > -1; i--) {
